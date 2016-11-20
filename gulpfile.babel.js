de6180aebab9f9c1f5cb0128f6608cfeb9051393
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-20 21:52:05 +1100

    Added Gulp task to compile and bundle JS using Babel.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,27 @@
-import gulp from 'gulp'
+import gulp from 'gulp';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
 
-gulp.task('default', () => console.log('Default task called'));
+var config = {
+    babel: {
+        presets: ['es2015']
+    },
+    js: {
+        src: './app/main.js',
+        outputFilename: 'bundle.js',
+        outputDir: './public/js'
+    }
+};
+
+gulp.task('browserify', () => {
+    var bundler = browserify(config.js.src);
+    bundler.transform(babelify, config.babel);
+
+    bundler.bundle()
+        .on('error', error => console.error(error))
+        .pipe(source(config.js.outputFilename))
+        .pipe(gulp.dest(config.js.outputDir));
+});
+
+gulp.task('default', ['browserify']);
