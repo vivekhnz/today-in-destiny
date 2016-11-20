7dbeaf8d169b9981547521f1360f3b11ec8170fb
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-21 02:19:05 +1100

    Added reference to JS bundle in new index.html.

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-var Express = require('express')
-var React = require('react')
-var ReactDOM = require('react-dom/server')
-var App = require('./app/components/App.js')
+var Express = require('express');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var swig = require('swig');
+var App = require('./app/components/App.js');
 
-var app = Express()
-const PORT = process.env.PORT || 3000
+var app = Express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-    var element = React.createElement(App)
-    var markup = ReactDOM.renderToString(element)
-    response.send(markup)
-})
+app.use('/bundle.js', (request, response) => {
+    response.sendFile(__dirname + '/public/js/bundle.js');
+});
+
+app.use('/', (request, response) => {
+    var element = React.createElement(App);
+    var html = ReactDOM.renderToString(element);
+    var page = swig.renderFile('views/index.html', { html: html });
+    response.setHeader('Content-Type', 'text/html');
+    response.end(page);
+});
 
 app.listen(PORT, error => {
     if (error) {
-        return console.log("An error occurred.", error)
+        return console.log("An error occurred.", error);
     }
-    
-    console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
