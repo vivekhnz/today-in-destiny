3025a715ffb1ee334ddf215381b7d451668bb0ad
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-23 01:28:03 +1100

    Added placeholder advisors JSX.

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,7 +3,104 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <div>Today in Destiny</div>
+            <div>
+                <div>
+                    <div>
+                        <p>Today in</p>
+                        <p>Destiny</p>
+                    </div>
+                    <div>
+                        <p>Nov</p>
+                        <p>18</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Events</p>
+                    <ul>
+                        <li>
+                            <p>Agent of the Nine</p>
+                            <p>Xûr has arrived...</p>
+                            <p>22h 23m</p>
+                            <ul>
+                                <li />
+                                <li />
+                                <li />
+                                <li />
+                                <li />
+                            </ul>
+                        </li>
+                        <li>
+                            <p>Trials of Osiris</p>
+                            <p>The Burning Shrine</p>
+                            <p>2d 22h 23m</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <p>Today</p>
+                    <ul>
+                        <li>
+                            <p>Daily Story Mission</p>
+                            <p>Enemy of My Enemy</p>
+                            <p>22h 23m</p>
+                        </li>
+                        <li>
+                            <p>Daily Crucible Playlist</p>
+                            <p>Control</p>
+                            <p>22h 23m</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <p>This Week</p>
+                    <ul>
+                        <li>
+                            <p>Raid</p>
+                            <p>Wrath of the Machine</p>
+                            <p>2d 22h 23m</p>
+                            <ul>
+                                <li>Vosik Challenge</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p>Nightfall Strike</p>
+                            <p>The Wretched Eye</p>
+                            <p>2d 22h 23m</p>
+                            <ul>
+                                <li>Epic</li>
+                                <li>Arc Burn</li>
+                                <li>Brawler</li>
+                                <li>Ironclad</li>
+                                <li>Exposure</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p>Heroic Strike Playlist</p>
+                            <p>SIVA Crisis Heroic</p>
+                            <p>2d 22h 23m</p>
+                            <ul>
+                                <li>Heroic</li>
+                                <li>Specialist</li>
+                                <li>Exposure</li>
+                                <li>Airborne</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p>Weekly Crucible Playlist</p>
+                            <p>Inferno Supremacy</p>
+                            <p>2d 22h 23m</p>
+                        </li>
+                        <li>
+                            <p>Raid</p>
+                            <p>King's Fall</p>
+                            <p>2d 22h 23m</p>
+                            <ul>
+                                <li>Warpriest Challenge</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     };
 }
