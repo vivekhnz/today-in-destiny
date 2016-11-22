5046a54251f3f1b8ea19077cea60387ab8bd59b8
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-22 20:10:09 +1100

    Split dependencies into their own bundle.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,43 +4,56 @@ import babel from 'gulp-babel';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 
+var config = {
+    srcDir: 'src',
+    outDir: 'build',
+    appDependencies: ['react', 'react-dom'],
+
+    babel: {
+        presets: ['es2015', 'react']
+    },
+    bundles: {}
+}
+
+config.bundles.app = {
+    entry: path.join(config.outDir, 'app/main.js'),
+    external: config.appDependencies,
+    output: 'public/app.bundle.js'  
+};
+config.bundles.vendor = {
+    require: config.appDependencies,
+    output: 'public/vendor.bundle.js'
+};
+
 function files(directory, extension) {
     return path.join(directory, `/**/**.${extension}`);
 }
 
 function copy(directoryName) {
     gulp.src(files(path.join(config.srcDir, directoryName), "*"))
+        .on('error', error => console.error(error))
         .pipe(gulp.dest(path.join(config.outDir, directoryName)));
 }
 
-var config = {
-    srcDir: 'src',
-    outDir: 'build',
-    
-    babel: {
-        presets: ['es2015', 'react']
-    },
-    browserify: {
-        entry: 'app/main.js',
-        output: 'public/bundle.js'
-    }
-};
+function bundle({ entry, require, external, output }) {
+    var bundler = entry ? browserify(entry) : browserify();
+    if (require) bundler = bundler.require(require);
+    if (external) bundler = bundler.external(external);
+    return bundler.bundle()
+        .on('error', error => console.error(error))
+        .pipe(source(output))
+        .pipe(gulp.dest(config.outDir));
+}
 
 gulp.task('babel', () => {
     gulp.src(files(config.srcDir, "js"))
+        .on('error', error => console.error(error))
         .pipe(babel(config.babel))
         .pipe(gulp.dest(config.outDir));
 });
 
 gulp.task('views', () => copy('views'));
+gulp.task('browserify', () => bundle(config.bundles.app));
+gulp.task('browserify-vendor', () => bundle(config.bundles.vendor));
 
-gulp.task('browserify', () => {
-    var entryPoint = path.join(config.outDir, config.browserify.entry);
-    browserify(entryPoint)
-        .bundle()
-        .on('error', error => console.error(error))
-        .pipe(source(config.browserify.output))
-        .pipe(gulp.dest(config.outDir));
-});
-
-gulp.task('default', ['babel', 'views', 'browserify']);
+gulp.task('default', ['babel', 'views', 'browserify', 'browserify-vendor']);
