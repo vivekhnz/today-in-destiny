4fb240dfe2e087db337fa5010c7b2242845e68d3
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-27 02:48:58 +1100

    Moved mock advisor data into new AdvisorsService.

--- src/app/actions/AdvisorsActions.js
+++ src/app/actions/AdvisorsActions.js
@@ -1,72 +1,5 @@
 import alt from '../alt';
-
-const events = [
-    {
-        type: "Agent of the Nine",
-        name: "Xûr has arrived...",
-        timeRemaining: "22h 23m",
-        items: [
-            "Hard Light",
-            "An Insurmountable Skullfort",
-            "Young Ahamkara's Spine",
-            "Apotheosis Veil",
-            "Three of Coins"
-        ]
-    },
-    {
-        type: "Trials of Osiris",
-        name: "The Burning Shrine",
-        timeRemaining: "2d 22h 23m"
-    }
-];
-const today = [
-    {
-        type: "Daily Story Mission",
-        name: "Enemy of My Enemy",
-        timeRemaining: "22h 23m"
-    },
-    {
-        type: "Daily Crucible Playlist",
-        name: "Control",
-        timeRemaining: "22h 23m"
-    }
-];
-const thisWeek = [
-    {
-        type: "Raid",
-        name: "Wrath of the Machine",
-        timeRemaining: "2d 22h 23m",
-        modifiers: ["Vosik Challenge"]
-    },
-    {
-        type: "Nightfall Strike",
-        name: "The Wretched Eye",
-        timeRemaining: "2d 22h 23m",
-        modifiers: ["Epic", "Arc Burn", "Brawler", "Ironclad", "Exposure"]
-    },
-    {
-        type: "Heroic Strike Playlist",
-        name: "SIVA Crisis Heroic",
-        timeRemaining: "2d 22h 23m",
-        modifiers: ["Heroic", "Specialist", "Exposure", "Airborne"]
-    },
-    {
-        type: "Weekly Crucible Playlist",
-        name: "Inferno Supremacy",
-        timeRemaining: "2d 22h 23m"
-    },
-    {
-        type: "Raid",
-        name: "King's Fall",
-        timeRemaining: "2d 22h 23m",
-        modifiers: ["Warpriest Challenge"]
-    }
-];
-const mockData = [
-    { name: "Events", advisors: events },
-    { name: "Today", advisors: today },
-    { name: "This Week", advisors: thisWeek }
-];
+import AdvisorsService from '../services/AdvisorsService';
 
 class AdvisorsActions {
     constructor() {
@@ -76,9 +9,9 @@ class AdvisorsActions {
     }
 
     fetchAdvisors() {
-        setTimeout(() => {
-            this.fetchAdvisorsSuccess(mockData);
-        }, 750);
+        AdvisorsService.fetchAdvisors()
+            .then(data => this.fetchAdvisorsSuccess(data))
+            .catch(() => console.log("Couldn't load advisors."));
     }
 }
 
---
+++ src/app/services/AdvisorsService.js
@@ -0,0 +1,77 @@
+const events = [
+    {
+        type: "Agent of the Nine",
+        name: "Xûr has arrived...",
+        timeRemaining: "22h 23m",
+        items: [
+            "Hard Light",
+            "An Insurmountable Skullfort",
+            "Young Ahamkara's Spine",
+            "Apotheosis Veil",
+            "Three of Coins"
+        ]
+    },
+    {
+        type: "Trials of Osiris",
+        name: "The Burning Shrine",
+        timeRemaining: "2d 22h 23m"
+    }
+];
+const today = [
+    {
+        type: "Daily Story Mission",
+        name: "Enemy of My Enemy",
+        timeRemaining: "22h 23m"
+    },
+    {
+        type: "Daily Crucible Playlist",
+        name: "Control",
+        timeRemaining: "22h 23m"
+    }
+];
+const thisWeek = [
+    {
+        type: "Raid",
+        name: "Wrath of the Machine",
+        timeRemaining: "2d 22h 23m",
+        modifiers: ["Vosik Challenge"]
+    },
+    {
+        type: "Nightfall Strike",
+        name: "The Wretched Eye",
+        timeRemaining: "2d 22h 23m",
+        modifiers: ["Epic", "Arc Burn", "Brawler", "Ironclad", "Exposure"]
+    },
+    {
+        type: "Heroic Strike Playlist",
+        name: "SIVA Crisis Heroic",
+        timeRemaining: "2d 22h 23m",
+        modifiers: ["Heroic", "Specialist", "Exposure", "Airborne"]
+    },
+    {
+        type: "Weekly Crucible Playlist",
+        name: "Inferno Supremacy",
+        timeRemaining: "2d 22h 23m"
+    },
+    {
+        type: "Raid",
+        name: "King's Fall",
+        timeRemaining: "2d 22h 23m",
+        modifiers: ["Warpriest Challenge"]
+    }
+];
+const mockData = [
+    { name: "Events", advisors: events },
+    { name: "Today", advisors: today },
+    { name: "This Week", advisors: thisWeek }
+];
+
+class AdvisorsService {
+    fetchAdvisors() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(mockData), 750);
+        });
+    }
+}
+
+export default new AdvisorsService();
