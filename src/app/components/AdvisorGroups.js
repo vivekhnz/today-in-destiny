afd9dfe00eb470fb8edaafac0698a80e0d69d95f
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-27 02:11:01 +1100

    Moved AdvisorGroups into AltContainer.

--- src/app/components/AdvisorGroups.js
+++ src/app/components/AdvisorGroups.js
@@ -1,41 +1,23 @@
 import React from 'react';
-import AdvisorsStore from '../stores/AdvisorsStore';
-import AdvisorsActions from '../actions/AdvisorsActions';
 import AdvisorGroup from './AdvisorGroup';
 
 class AdvisorGroups extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = AdvisorsStore.getState();
-        this.onChange = this.onChange.bind(this);
-    }
-
     componentDidMount() {
-        AdvisorsStore.listen(this.onChange);
-        AdvisorsActions.fetchAdvisors();
-    }
-
-    componentWillUnmount() {
-        AdvisorsStore.unlisten(this.onChange);
-    }
-
-    onChange(state) {
-        this.setState(state);
+        this.props.fetchAdvisors();
     }
 
     render() {
-        if (this.state.errorMessage) {
-            return <div>{this.state.errorMessage}</div>;
+        if (this.props.errorMessage) {
+            return <div>{this.props.errorMessage}</div>;
         }
-        if (!this.state.advisorGroups.length) {
+        if (!this.props.advisorGroups.length) {
             return <div>Loading...</div>;
         }
 
         return (
             <div>
                 {
-                    this.state.advisorGroups.map((group, i) =>
+                    this.props.advisorGroups.map((group, i) =>
                         <AdvisorGroup key={i}
                             name={group.name}
                             advisors={group.advisors} />)
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import AltContainer from 'alt-container';
+
 import Header from './Header';
 import AdvisorGroups from './AdvisorGroups';
+
+import AdvisorsStore from '../stores/AdvisorsStore';
+import AdvisorsActions from '../actions/AdvisorsActions';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
-                <Header month="Nov" day="18" /> 
-                <AdvisorGroups />
+                <Header month="Nov" day="18" />
+                <AltContainer
+                    store={AdvisorsStore}
+                    actions={AdvisorsActions}
+                    component={AdvisorGroups} />
             </div>
         );
     };
