d0804d781a5011cb1c8359b698c097819c35e359
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-28 03:30:31 +1100

    Adjusted margins and spacing.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,13 +4,13 @@ class Header extends React.Component {
     render() {
         return (
             <div className="headerContainer">
-                <div>
-                    <p className="headerLine1">Today in</p>
-                    <p className="headerLine2">Destiny</p>
-                </div>
                 <div className="dateContainer">
                     <p className="dateMonth">{this.props.date.month}</p>
                     <p className="dateDay">{this.props.date.day}</p>
+                </div>
+                <div className="headerTextContainer">
+                    <p className="headerLine1">Today in</p>
+                    <p className="headerLine2">Destiny</p>
                 </div>
             </div>
         );
