c980ad98bb4172556f693c59fc7ae8a78a01180f
Vivek Hari <vivek.hari@outlook.co.nz>
2016-11-28 02:34:49 +1100

    Replaced CSS stylesheet with LESS.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,10 @@ import babel from 'gulp-babel';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
+import plumber from 'gulp-plumber';
 import uglify from 'gulp-uglify';
+import less from 'gulp-less';
+import cssmin from 'gulp-cssmin';
 
 var config = {
     srcDir: 'src',
@@ -31,6 +34,10 @@ config.bundles.app = {
 config.bundles.vendor = {
     require: config.appDependencies,
     output: 'public/vendor.bundle.js'
+};
+config.stylesheets = {
+    srcDir: path.join(config.srcDir, 'public/stylesheets'),
+    outDir: path.join(config.outDir, 'public/stylesheets')
 };
 
 function files(directory, extension) {
@@ -61,10 +68,15 @@ gulp.task('babel', () => {
         .pipe(babel(config.babel))
         .pipe(gulp.dest(config.outDir));
 });
-
 gulp.task('views', () => copy('views'));
 gulp.task('fonts', () => copy('public/fonts'));
-gulp.task('stylesheets', () => copy('public/stylesheets'));
+gulp.task('stylesheets', () => {
+    return gulp.src(files(config.stylesheets.srcDir, "less"))
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulpif(production, cssmin()))
+        .pipe(gulp.dest(config.stylesheets.outDir));
+});
 gulp.task('browserify', ['babel'], () => bundle(config.bundles.app));
 gulp.task('browserify-vendor', ['babel'], () => bundle(config.bundles.vendor));
 
