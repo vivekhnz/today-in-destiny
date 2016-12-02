5e0232a8908df739e67345676438b9f7e436dbd3
Vivek Hari <vivek.hari@outlook.co.nz>
2016-12-02 21:28:23 +1100

    Auto-restart server on server code changes. Auto-reload browser on public asset changes.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,140 +9,176 @@ import plumber from 'gulp-plumber';
 import uglify from 'gulp-uglify';
 import less from 'gulp-less';
 import cssmin from 'gulp-cssmin';
+import es from 'event-stream';
+import nodemon from 'gulp-nodemon';
 import browserSync from 'browser-sync';
 let sync = browserSync.create();
 
-// build functions
-function files(directory, extension) {
-    return path.join(directory, `/**/**.${extension}`);
-}
+var production = process.env.NODE_ENV === 'production';
 
-function build(buildTask) {
-    return () => {
-        var {name, input, src, extension, dest, actions} = buildTask();
-        let stream = src && extension ? gulp.src(files(src, extension)) : input;
-        if (!stream) {
-            console.error("No input or source + extension provided.");
-        }
-        stream = stream.on('error', error => console.error(`${name}: ${error}`));
-        if (actions) {
-            actions.forEach(action => {
-                stream = stream.pipe(action);
-            }, this);
-        }
-        return stream.pipe(gulp.dest(dest));
-    };
-}
-
-function watch({directory, extension, tasks}) {
-    return () => gulp.watch([files(directory, extension)], tasks);
+function join(base, paths) {
+    return paths.map(fn => path.join(base, fn));
 }
 
 // configuration
-var production = process.env.NODE_ENV === 'production';
-
 var config = {
-    srcDir: 'src',
-    outDir: 'build',
     appDependencies: [
         'react', 'react-dom', 'react-router',
         'alt', 'alt-container', 'iso'
     ],
-    babel: {
-        presets: ['es2015', 'react']
+    publicAssets: ['build/public/**/**.*'],
+    utils: {
+        babel: {
+            presets: ['es2015', 'react']
+        },
+        browserSync: {
+            proxy: 'localhost:3000',
+            port: 4000,
+        }
     },
-    bundles: {}
-}
-
-config.bundles.app = {
-    entry: path.join(config.outDir, 'app/main.js'),
-    external: config.appDependencies,
-    output: 'public/app.bundle.js'
-};
-config.bundles.vendor = {
-    require: config.appDependencies,
-    output: 'public/vendor.bundle.js'
-};
-config.stylesheets = {
-    srcDir: path.join(config.srcDir, 'public/stylesheets'),
-    outDir: path.join(config.outDir, 'public/stylesheets')
-};
-
-// task definitions
-var tasks = {};
-tasks.babel = function () {
-    return {
-        name: 'babel',
-        src: config.srcDir,
-        extension: "js",
-        dest: config.outDir,
-        actions: [babel(config.babel)]
-    };
-}
-tasks.stylesheets = function () {
-    return {
-        name: 'stylesheets',
-        src: config.stylesheets.srcDir,
-        extension: "less",
-        dest: config.stylesheets.outDir,
-        actions: [
-            plumber(), less(),
-            gulpif(production, cssmin())
-        ]
-    };
-};
-tasks.copy = function (directoryName) {
-    return () => {
-        return {
-            name: `copy '${directoryName}'`,
-            src: path.join(config.srcDir, directoryName),
-            extension: "*",
-            dest: path.join(config.outDir, directoryName)
-        };
-    };
-};
-tasks.bundle = function bundle({ entry, require, external, output }) {
-    return () => {
-        var bundler = entry ? browserify(entry) : browserify();
-        if (require) bundler = bundler.require(require);
-        if (external) bundler = bundler.external(external);
-        return {
-            name: `bundle '${output}'`,
-            input: bundler.bundle(),
-            dest: config.outDir,
-            actions: [
-                source(output), buffer(),
-                gulpif(production, uglify({ mangle: false }))
-            ]
-        };
-    };
-}
-
-// watchers
-var watchers = {
+    copyToOutput: [
+        'views', 'public/fonts'
+    ],
     stylesheets: {
-        directory: config.stylesheets.srcDir,
-        extension: "less",
-        tasks: ['stylesheets', 'reload']
+        src: 'src/public/stylesheets/**/**.less',
+        outDir: 'build/public/stylesheets'
+    },
+    server: {
+        entry: 'build/server.js',
+        dependencies: [
+            'server.js', 'routes.js', 'services/**/**.js'
+        ]
+    }
+}
+config.js = {
+    all: 'src/**/**.js',
+    server: join('src', config.server.dependencies)
+};
+config.server.nodemon = {
+    script: config.server.entry,
+    watch: join('build', config.server.dependencies),
+    ext: 'js',
+    delay: 2000
+};
+config.bundles = {
+    vendor: {
+        require: config.appDependencies,
+        output: 'public/vendor.bundle.js'
+    },
+    app: {
+        entry: 'build/app/main.js',
+        external: config.appDependencies,
+        output: 'public/app.bundle.js'
     }
 };
 
-// tasks
-gulp.task('babel', build(tasks.babel));
-gulp.task('views', build(tasks.copy('views')));
-gulp.task('fonts', build(tasks.copy('public/fonts')));
-gulp.task('stylesheets', build(tasks.stylesheets));
-gulp.task('browserify', ['babel'], build(tasks.bundle(config.bundles.app)));
-gulp.task('browserify-vendor', ['babel'], build(tasks.bundle(config.bundles.vendor)));
+// compile ES2015 JS
+function getDestination(src) {
+    let rootIndex = src.indexOf("src\\");
+    let withoutSrc = path.dirname(
+        rootIndex == -1 ? src : src.substring(rootIndex + 4));
+    let dest = path.join('build', withoutSrc);
 
-gulp.task('sync', () => {
-    sync.init({
-        proxy: "localhost:3000"
-    });
+    while ("\\*".includes(dest.charAt(dest.length - 1))) {
+        dest = dest.slice(0, -1);
+    }
+    return dest;
+}
+function compileJS(src, dest = null) {
+    return gulp.src(src)
+        .on('error', error => console.error(error))
+        .pipe(babel(config.utils.babel))
+        .pipe(gulp.dest(dest || getDestination(src)));
+}
+gulp.task('babel-server', () => {
+    return es.merge(config.js.server.map(dep => compileJS(dep)));
 });
-gulp.task('reload', () => sync.reload());
-gulp.task('stylesheets-watch', watch(watchers.stylesheets));
+gulp.task('babel-app', () => {
+    let sources = config.js.server.map(dep => `!${dep}`);
+    sources.splice(0, 0, config.js.all);
+    return compileJS(sources, 'build');
+});
+gulp.task('babel-all', ['babel-server', 'babel-app']);
 
-gulp.task('core', ['sync', 'babel', 'views', 'fonts', 'stylesheets']);
-gulp.task('build', ['core', 'browserify', 'browserify-vendor']);
-gulp.task('default', ['core', 'browserify', 'browserify-vendor', 'stylesheets-watch']);
+// compile LESS stylesheets
+gulp.task('stylesheets', () => {
+    return gulp.src(config.stylesheets.src)
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulpif(production, cssmin()))
+        .pipe(gulp.dest(config.stylesheets.outDir));
+});
+
+// copy files that don't require compilation
+function copy(dir) {
+    return gulp.src(`src/${dir}/**/**.*`)
+        .on('error', error => console.error(error))
+        .pipe(gulp.dest(`build/${dir}`));
+}
+gulp.task('copy', () => {
+    return es.merge(config.copyToOutput.map(dir => copy(dir)));
+});
+
+// generate bundles
+function bundle({ entry, require, external, output }) {
+    var bundler = entry ? browserify(entry) : browserify();
+    if (require) bundler = bundler.require(require);
+    if (external) bundler = bundler.external(external);
+    return bundler.bundle()
+        .on('error', error => console.error(error))
+        .pipe(source(output))
+        .pipe(buffer())
+        .pipe(gulpif(production, uglify({ mangle: false })))
+        .pipe(gulp.dest('build'));
+}
+gulp.task('bundle-vendor', ['babel-app'], () => bundle(config.bundles.vendor));
+gulp.task('bundle-app', ['babel-app'], () => bundle(config.bundles.app));
+
+// launch server and auto-restart whenever server code is changed
+gulp.task('server', ['build'], callback => {
+    var started = false; 
+    return nodemon(config.server.nodemon)
+        .on('start', () => {
+            if (!started) {
+                callback();
+            }
+            started = true;
+        })
+        .on('error', error => { throw error; });
+});
+
+// initialize BrowserSync
+gulp.task('sync', () => {
+    sync.init(config.utils.browserSync);
+});
+
+// reload browser
+gulp.task('reload', () => sync.reload());
+
+// automatically recompile stylesheets
+gulp.task('stylesheets-watch', () => {
+    gulp.watch([config.stylesheets.src], ['stylesheets']);
+});
+
+// automatically recompile server JS
+gulp.task('server-watch', () => {
+    gulp.watch(config.js.server, ['babel-server']);
+});
+
+// automatically reload browser whenever public assets change
+gulp.task('reload-watch', () => {
+    gulp.watch(config.publicAssets, ['reload']);
+});
+
+// execute all build tasks
+gulp.task('build', [
+    'babel-all', 'copy', 'stylesheets', 'bundle-vendor', 'bundle-app'
+]);
+
+// build and start server
+gulp.task('serve', ['build', 'server']);
+
+// start BrowserSync and watch for changes
+gulp.task('watch', [
+    'sync', 'stylesheets-watch', 'server-watch', 'reload-watch'
+]);
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route} from 'react-router';
-import App from './components/App';
-import Home from './components/Home';
+import App from './app/components/App';
+import Home from './app/components/Home';
 
 export default (
     <Route component={App}>
