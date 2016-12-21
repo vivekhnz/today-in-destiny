635e0515f1eea5bd8157469fd8fed060efb56101
Vivek Hari <vivek.hari@outlook.co.nz>
2016-12-21 19:43:07 +1100

    Styled hero content.

--- src/app/components/Activity.js
+++ src/app/components/Activity.js
@@ -7,15 +7,30 @@ import SmallHeader from './SmallHeader';
 import ActivityStore from '../stores/ActivityStore';
 import ActivityActions from '../actions/ActivityActions';
 
+var overlayColor = 'rgba(39, 58, 65, 0.75)';
+
 class Activity extends React.Component {
     render() {
+        let heroImage = '/images/advisors/backgrounds/default.jpg';
+        let heroImageStyle = {
+            background: heroImage
+                ? `linear-gradient(${overlayColor}, ${overlayColor}), url('${heroImage}')`
+                : overlayColor
+        };
+        let iconStyle = {
+            backgroundImage: "url('/images/advisors/icons/default.png')"
+        };
         return (
             <div>
-                <div className="activityHeroImage">
+                <div className="activityHeroImage" style={heroImageStyle}>
                     <SmallHeader date={this.props.date} />
-                    <div className="activityHeroContent">
-                        <div className="advisorContainer">
-                            <p className="advisorName">{this.props.params.id}</p>
+                    <div className="activityHeroWrapper">
+                        <div className="activityHeroContainer">
+                            <div className="activityIcon" style={iconStyle}></div>
+                            <div className="activityHeroContent">
+                                <p className="activityType">{this.props.params.id}</p>
+                                <p className="activityName">{this.props.params.id}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
