ffc970e960dc848fec2d96a330808848e36d8bc6
Vivek Hari <vivek.hari@outlook.co.nz>
2017-02-06 20:37:10 +1100

    Added support for custom fonts.

--- src/bot/twitterBot.js
+++ src/bot/twitterBot.js
@@ -1,13 +1,16 @@
 import fs from 'fs';
 import swig from 'swig';
 import webshot from 'webshot';
+import rimraf from 'rimraf';
 
 let TASKS = {
     'weekly': postWeeklyActivities
 };
 
-let HTML_PATH = __dirname + '/../views/twitterBot.html';
-let CSS_PATH = __dirname + '/../public/stylesheets/bot.css';
+let SWIG_VIEW_PATH = `${__dirname}/../views/twitterBot.html`;
+let CSS_PATH = `${__dirname}/../public/stylesheets/bot.css`;
+let OUTPUT_HTML_FILE = `${__dirname}/bot.html`;
+let OUTPUT_IMAGE = 'build/bot/weekly.png';
 
 if (process.argv.length >= 3) {
     let taskID = process.argv[2];
@@ -24,13 +27,11 @@ else {
 }
 
 function postWeeklyActivities() {
-    loadCSS().then(css => {
-        let content = {
-            content: 'Rendered by bot.'
-        };
-        let html = swig.renderFile(HTML_PATH, content);
-        screenshot(html, css, 'build/weekly.png');
-    }).catch(error => console.log("Couldn't post weekly activities."));
+    loadCSS()
+        .then(createPage)
+        .then(screenshot)
+        .then(cleanup)
+        .catch(error => console.log("Couldn't post weekly activities."));
 }
 
 function loadCSS() {
@@ -41,24 +42,94 @@ function loadCSS() {
                 reject(error);
             }
             else {
-                resolve(data);
+                // inject absolute fonts directory
+                let dir = __dirname.replace(/\\/g, '/');
+                let absolute = data.replace(/FONTS_DIR/g,
+                    `${dir}/../public/fonts`);
+                resolve(absolute);
             }
         });
     });
 }
 
-function screenshot(html, css, output) {
-    let options = {
-        siteType: 'html',
-        customCSS: css,
-        errorIfJSException: true
+function createPage(css) {
+    return new Promise((resolve, reject) => {
+        let html = generateHTML(css);
+        console.log('HTML generated. Saving file...');
+        fs.writeFile(OUTPUT_HTML_FILE, html, error => {
+            if (error) {
+                console.log('HTML file could not be saved.');
+                reject(error);
+            }
+            else {
+                console.log('HTML file saved.');
+                resolve(OUTPUT_HTML_FILE);
+            }
+        });
+    });
+}
+
+function generateHTML(css) {
+    let content = {
+        css: css,
+        cardName: 'This Week',
+        date: 'Jan 31 - Feb 6',
+        advisors: [
+            {
+                name: 'Aksis Challenge',
+                type: 'Wrath of the Machine',
+            },
+            {
+                name: "Winter's Run",
+                type: 'Nightfall Strike',
+            },
+            {
+                name: 'SIVA Crisis Heroic',
+                type: 'Heroic Strike Playlist',
+            },
+            {
+                name: 'Mayhem Clash',
+                type: 'Weekly Crucible Playlist',
+            }
+        ]
     };
-    webshot(html, output, options, error => {
-        if (error) {
-            console.log(`Webshot ${error}`);
-        }
-        else {
-            console.log('Webshot completed successfully.');
-        }
+    return swig.renderFile(SWIG_VIEW_PATH, content);
+}
+
+function screenshot(file) {
+    return new Promise((resolve, reject) => {
+        let options = {
+            errorIfJSException: true,
+            windowSize: {
+                width: 892,
+                height: 512
+            }
+        };
+        webshot(file, OUTPUT_IMAGE, options, error => {
+            if (error) {
+                console.log(`Webshot ${error}`);
+                reject(error);
+            }
+            else {
+                console.log('Webshot completed successfully.');
+                resolve();
+            }
+        });
+    });
+}
+
+function cleanup() {
+    return new Promise((resolve, reject) => {
+        rimraf(OUTPUT_HTML_FILE, error => {
+            if (error) {
+                console.log("Couldn't delete generated HTML page:");
+                console.log(error);
+                reject(error);
+            }
+            else {
+                console.log('Deleted generated HTML page.');
+                resolve();
+            }
+        });
     });
 }
