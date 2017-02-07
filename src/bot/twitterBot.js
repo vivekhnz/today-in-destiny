a667a6aead8a2ce6bb13acb8d77d31f232ff023d
Vivek Hari <vivek.hari@outlook.co.nz>
2017-02-08 00:19:30 +1100

    Added logic to tweet generated image.

--- src/bot/twitterBot.js
+++ src/bot/twitterBot.js
@@ -12,8 +12,8 @@ import fs from 'fs';
 import request from 'request';
 import swig from 'swig';
 import webshot from 'webshot';
-import Twitter from 'twitter';
 import rimraf from 'rimraf';
+import { default as twitter } from '../services/twitter';
 import { default as time } from '../services/time';
 
 let TASKS = {
@@ -31,8 +31,9 @@ let WEEKLY_CARD = {
 let BASE_DIR = __dirname.replace(/\\/g, '/');
 let SWIG_VIEW_PATH = `${__dirname}/../views/twitterBot.html`;
 let CSS_PATH = `${__dirname}/../public/stylesheets/bot.css`;
-let OUTPUT_HTML_FILE = `${__dirname}/bot.html`;
-let OUTPUT_IMAGE = 'build/bot/weekly.png';
+let OUTPUT_DIR = `${__dirname}/temp`;
+let OUTPUT_HTML_FILE = `${OUTPUT_DIR}/output.html`;
+let OUTPUT_IMAGE = `${OUTPUT_DIR}/output.png`;
 
 if (process.argv.length >= 3) {
     let taskID = process.argv[2];
@@ -49,10 +50,14 @@ else {
 }
 
 function postWeeklyActivities() {
+    let tweetText = '#Destiny';
     Promise.all([retry(getAdvisors, 5), loadCSS()])
         .then(createPage)
-        .then(screenshot)
-        .then(tweet)
+        .then(content => {
+            tweetText = content.tweetText || tweetText;
+            return screenshot(content.outputFile);
+        })
+        .then(file => tweet(tweetText, file))
         .then(cleanup)
         .catch(error => console.log("Couldn't post weekly activities."));
 }
@@ -128,6 +133,10 @@ function createPage([data, css]) {
             content.css = css;
             let html = swig.renderFile(SWIG_VIEW_PATH, content);
 
+            if (!fs.existsSync(OUTPUT_DIR)) {
+                fs.mkdirSync(OUTPUT_DIR);
+            }
+
             console.log('HTML generated. Saving file...');
             fs.writeFile(OUTPUT_HTML_FILE, html, error => {
                 if (error) {
@@ -136,7 +145,10 @@ function createPage([data, css]) {
                 }
                 else {
                     console.log('HTML file saved.');
-                    resolve(OUTPUT_HTML_FILE);
+                    resolve({
+                        outputFile: OUTPUT_HTML_FILE,
+                        tweetText: `${content.cardName} in #Destiny (${content.date})`
+                    });
                 }
             });
         }
@@ -220,30 +232,41 @@ function screenshot(file) {
             }
             else {
                 console.log('Webshot completed successfully.');
-                resolve();
+                resolve(OUTPUT_IMAGE);
             }
         });
     });
 }
 
-function tweet() {
-    let content = {
-        status: 'Test tweet from bot script'
-    };
-
-    let twitter = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+function loadImage(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (error, data) => {
+            if (error) {
+                console.log("Couldn't load image.");
+                reject(error);
+            }
+            else {
+                resolve(data);
+            }
+        });
     });
-    return twitter.post('statuses/update', content)
-        .then(tweet => console.log('Tweet posted.'));
+}
+
+function tweet(text, imagePath) {
+    let promise = null;
+    if (imagePath) {
+        promise = loadImage(imagePath)
+            .then(media => twitter.tweet(text, media));
+    }
+    else {
+        promise = twitter.tweet(text, null);
+    }
+    return promise.then(() => console.log('Tweet posted.'));
 }
 
 function cleanup() {
     return new Promise((resolve, reject) => {
-        rimraf(OUTPUT_HTML_FILE, error => {
+        rimraf(OUTPUT_DIR, error => {
             if (error) {
                 console.log("Couldn't delete generated HTML page:");
                 console.log(error);
